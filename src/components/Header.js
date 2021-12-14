d9de781417e493e3b0ba8111f81c70f79b996dce
Connor Hansen <chansen13@georgefox.edu>
2021-12-14 15:01:52 -0800

    WIP conditional header rendering.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,95 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { withFirestore, isLoaded } from 'react-redux-firebase';
+import 'firebase/compat/auth';
+import { connect } from 'react-redux';
+import { getAuth } from "firebase/auth";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
-function Header(){
-  return (
-    <React.Fragment>
-      <h1> Red Oak Farm</h1>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/sign-in">Sign In</Link>
-        </li>
-        <li>
-          <Link to="/sign-up">Sign Up</Link>
-        </li>
-        <li>
-          <Link to="/sign-out">Sign Out</Link>
-        </li>
-      </ul>
-    </React.Fragment>
-  );
+
+
+// class Header extends React.Component {
+
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       selectedReservation: null,
+//       editing: false
+//     };
+//   }
+function Header() {
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  // render() {
+  //   const auth = this.props.firebase.auth();
+  //   if (!isLoaded(auth)) {
+  //     return (
+  //       <React.Fragment>
+  //         <h1>Loading...</h1>
+  //       </React.Fragment>
+  //     )
+  //   }
+  //   if ((isLoaded(auth)) && (auth.currentUser == null)) {
+  if (user === null) {
+    return (
+      <React.Fragment>
+        <h1> Red Oak Farm</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-in">Sign In</Link>
+          </li>
+          <li>
+            <Link to="/sign-up">Sign Up</Link>
+          </li>
+          <li>
+            <Link to="/sign-out">Sign Out</Link>
+          </li>
+        </ul>
+      </React.Fragment>
+    )
+  } else if (user !== null){
+
+    return (
+
+
+
+      // if ((isLoaded(auth)) && (auth.currentUser != null)) {
+      // return (
+      <React.Fragment>
+        <h1> Red Oak Farm</h1>
+        <ul>
+          <p>Signed in as: {user.email}</p>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/sign-out">Sign Out</Link>
+          </li>
+        </ul>
+      </React.Fragment>
+    );
+  }
+  // }
 }
 
+// Header.propTypes = {
+//   mainReservationList: PropTypes.object
+// };
+
+// const mapStateToProps = state => {
+//   return {
+//     mainReservationList: state.mainReservationList,
+//     formVisibleOnPage: state.formVisibleOnPage
+//   }
+// }
+
+// Header = connect(mapStateToProps)(Header);
+
+// export default withFirestore(Header);
 export default Header;
